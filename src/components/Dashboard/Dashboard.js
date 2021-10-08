52b69e2deb205818c1f32fcf527fd416211ea104
lucianoleyriaa <lucholeyria@gmail.com>
2021-10-08 08:03:38 -0300

    Se resolvieron algunos errores

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,5 @@
 import { Route, useRouteMatch } from "react-router-dom";
 import { Fragment, useState } from "react";
-import Listar from "../Listar";
 // import estilos2 from "./estilos2.css";
 import Header from "../Header/Header";
 import Sidebar from "../Sidebar/Sidebar";
@@ -8,15 +7,13 @@ import Backdrop from "../Backdrop/Backdrop";
 import styles from "./Dashboard.module.css";
 import ListEmployees from "../Employees/List/ListEmployees";
 import NewEmployee from "../Employees/New/NewEmployee";
-import EditEmployee from "../Employees/Edit/EditEmployee"
+import EditEmployee from "../Employees/Edit/EditEmployee";
 import ListCustomers from "../Customers/List/ListCustomers";
 import NewCustomer from "../Customers/New/NewCustomer";
-import EditCustomer from "../Customers/Edit/EditCustomer"
-import Crear from "../Crear";
+import EditCustomer from "../Customers/Edit/EditCustomer";
 import ListTrucks from "../Trucks/List/ListTrucks";
 import NewTruck from "../Trucks/New/NewTruck";
 import EditTruck from "../Trucks/Edit/EditTruck";
-import EditEmployee from "../Employees/Edit/EditEmployee";
 
 function Dashboard() {
    const [backdropOpen, setOpen] = useState(false);
@@ -45,22 +42,17 @@ function Dashboard() {
                <NewEmployee />
             </Route>
             <Route path={`${path}/empleados/editar/:id`}>
-               <EditEmployee  />
+               <EditEmployee />
             </Route>
 
             <Route path={`${path}/clientes`} exact>
-               <ListEmployees />
+               <ListCustomers />
             </Route>
-            <Route path={`${path}/clientes/nuevo`}>
-               <NewEmployee />
+            <Route path={`${path}/clientes/nuevo`} exact>
+               <NewCustomer />
             </Route>
-            <Route path={`${path}/clientes/editar/:id`}>
-               <EditEmployee  />
-            <Route path={`${path}/empleados/editar/:id`} exact>
-               <EditEmployee />
-            </Route>
-            <Route path={`${path}/empleados/nuevo`}>
-               <NewEmployee />
+            <Route path={`${path}/clientes/editar/:id`} exact>
+               <EditCustomer />
             </Route>
             <Route path={`${path}/camiones`} exact>
                <ListTrucks />
