8bbd4e1aab544bd0c24cb8568b5ef2947ee1e4f3
lucianoleyriaa <lucholeyria@gmail.com>
2021-10-08 07:54:50 -0300

    Se creo un nuevo componente para cada campo de los formularios

---
+++ src/components/Employees/FormGroup.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const FormGroup = ({
+   labelName,
+   labelClass,
+   inputClass,
+   inputType,
+   inputName,
+   inputValue,
+   disabledOp,
+   onChangeHandler,
+   checkValid,
+   errors,
+}) => {
+   return (
+      <div className="form__group">
+         <label className={labelClass}>{labelName}: </label>
+         <input
+            className={`${inputClass} ${
+               !disabledOp ? "form__input__edit" : ""
+            } ${checkValid(errors[`${inputName}`]) ? "is-invalid" : ""}`}
+            type={inputType}
+            name={inputName}
+            value={inputValue}
+            disabled={disabledOp}
+            onChange={onChangeHandler}
+         />
+         <small id="helpId" className="invalid-feedback">
+            Debes ingresar un {inputName}
+         </small>
+      </div>
+   );
+};
+
+export default FormGroup;
